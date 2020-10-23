bf7bbb17574a48310059b959499448141f383b21
inventive-view-7245 <ori.markowitz@gmail.com>
2020-10-23 15:35:15 +0000

    Automatically backed up by Learn

--- lib/03_third_challenge.rb
+++ lib/03_third_challenge.rb
@@ -14,19 +14,15 @@ def third_challenge
         age: "15",
         status: "alive"
         },
-      :hero_friends => []
+      :hero_friends => [{
+        name: "Benvolio", age: "17", attitude: "worried"
+      }, {name: "Mercutio", age: "18", attitude: "hot-headed"}]
    }, 
    :capulet => {
-      :patriarch => {
-        #your key/value pairs here
-        },
-      :matriarch => {
-        #your key/value pairs here
-        },
-      :heroine => {
-        #your key/value pairs here
-        },
-      :heroine_friends => []
+      :patriarch => {name: "Lord Capulet", age: "50"},
+      :matriarch => {name: "Lady Capulet", age: "51"},
+      :heroine => {name: "Juliet", age: "15", status: "alive"},
+      :heroine_friends => [{name: "Steven", age: "30", attitude: "confused"}, {name: "Nurse", age: "44", attitude: "worried"}]
    }
   }
 
